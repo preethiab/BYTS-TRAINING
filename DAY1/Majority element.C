c83a38ce71893ffcec033788d30061c846ad580b
preethiab <107450753+preethiab@users.noreply.github.com>
2022-12-17 15:45:28 +0530

    Update Majority element.C

--- DAY1/Majority element.C
+++ DAY1/Majority element.C
@@ -1,9 +1,14 @@
 int majorityElement(int* nums, int numsSize){
-    int a=arr[0];
+    int a=nums[0];
     int aCount=1;
     for(int i=1;i<numsSize;i++){
-        if(arr[i]==a){
-            a=arr[i];
+        if(nums[i]==a){
+            ++aCount;        
+        }else{
+            --aCount;
+        }
+        if(aCount==0){
+            a=nums[i];
             aCount=1;
         }
     }
